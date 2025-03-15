e1300b6e19f50e45369386f2b24ee065d7bb9161
eliya-amanoeel <eliya.amanoeel@hotmail.com>
2025-03-15 11:56:30 +0100

    CSS updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <WiFiClient.h>
+#include <ArduinoOTA.h>
 #include <time.h>
 
 // Network credentials
@@ -20,6 +21,7 @@ Mode currentMode = Tuning; // Default mode
 // Function prototypes
 void setupWiFi();
 void setupServer();
+void setupOTA();
 void displayTime();
 void handleRoot();
 void handleSet();
@@ -34,10 +36,12 @@ void setup() {
 
     setupWiFi();
     setupServer();
+    setupOTA();
 }
 
 void loop() {
     server.handleClient();
+    ArduinoOTA.handle();
     if (currentMode == Clock) {
         displayTime();
     }
@@ -72,17 +76,55 @@ void setupServer() {
     server.begin();
 }
 
+// Function to set up OTA updates
+void setupOTA() {
+    ArduinoOTA.onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH) {
+            type = "sketch";
+        } else { // U_SPIFFS
+            type = "filesystem";
+        }
+        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+        Serial.println("Start updating " + type);
+    });
+    ArduinoOTA.onEnd([]() {
+        Serial.println("\nEnd");
+    });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+    ArduinoOTA.onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) {
+            Serial.println("Auth Failed");
+        } else if (error == OTA_BEGIN_ERROR) {
+            Serial.println("Begin Failed");
+        } else if (error == OTA_CONNECT_ERROR) {
+            Serial.println("Connect Failed");
+        } else if (error == OTA_RECEIVE_ERROR) {
+            Serial.println("Receive Failed");
+        } else if (error == OTA_END_ERROR) {
+            Serial.println("End Failed");
+        }
+    });
+    ArduinoOTA.begin();
+    Serial.println("OTA Ready");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
 // Function to handle the root endpoint
 void handleRoot() {
     String html = "<!DOCTYPE html><html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
     html += "<link rel=\"icon\" href=\"data:,\">";
     html += "<style>";
-    html += "html { font-family: 'Comic Sans MS', cursive, sans-serif; display: inline-block; margin: 0px auto; text-align: center; background-color: #f0f0f0;}";
+    html += "html { font-family: 'Helvetica', cursive, sans-serif; display: inline-block; margin: 0px auto; text-align: center; background-color: #f0f0f0;}";
     html += "h1 { color: #333; }";
-    html += ".button { border: 2px solid transparent; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer; background-color: #FF0000; border-radius: 12px; }";
+    html += ".button { border: 2px solid transparent; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer; background-color:rgb(0, 0, 0); border-radius: 12px; }";
     html += ".highlight { border: 2px solid yellow; }";
     html += ".hidden { display: none; }";
-    html += "#clock { font-size: 40px; font-weight: bold; color: #333; }";
+    html += "#clock { font-size: 60px; font-weight: bold; color: #333; }";
     html += ".switch { position: relative; display: inline-block; width: 60px; height: 34px; }";
     html += ".switch input { opacity: 0; width: 0; height: 0; }";
     html += ".slider { position: absolute; cursor: pointer; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc; transition: .4s; border-radius: 34px; }";
@@ -93,9 +135,10 @@ void handleRoot() {
     html += "input[type=range] { -webkit-appearance: none; width: 100%; margin: 10.8px 0; }";
     html += "input[type=range]:focus { outline: none; }";
     html += "input[type=range]::-webkit-slider-runnable-track { width: 100%; height: 8.4px; cursor: pointer; animate: 0.2s; background: #ddd; border-radius: 1.3px; }";
-    html += "input[type=range]::-webkit-slider-thumb { border: 1px solid #000000; height: 36px; width: 16px; border-radius: 3px; background: #FFFF00; cursor: pointer; -webkit-appearance: none; margin-top: -14px; }";
-    html += "@keyframes rotate { from { transform: rotate(0deg); } to { transform: rotate(360deg); } }";
-    html += ".rotate { display: inline-block; animation: rotate 2s linear infinite; }";
+    html += "input[type=range]::-webkit-slider-thumb { border: 1px solid #000000; height: 36px; width: 16px; border-radius: 3px; background:rgb(0, 0, 0); cursor: pointer; -webkit-appearance: none; margin-top: -14px; }";
+    html += ".loader { width: 40px; aspect-ratio: 1; border:2px solid; box-sizing: border-box; color: #000; background: radial-gradient(circle 3px, currentColor 95%,#0000), linear-gradient(      currentColor 50%,#0000 0) 50%/3px 80% no-repeat, linear-gradient(90deg,currentColor 50%,#0000 0) 50%/60% 3px no-repeat; position: relative;}";
+    html += ".loader:before { content: ''; position: absolute; height: 50px; inset: 100% 10% auto; background: radial-gradient(circle closest-side at 50% calc(100% - 10px), currentColor 94%,#0000), linear-gradient(currentColor 0 0) top/3px 80% no-repeat; transform-origin: top; animation: l2 2s infinite cubic-bezier(0.5,200,0.5,-200);}";
+    html += "@keyframes l2 { 100% {transform: rotate(0.4deg)}}";
     html += "</style>";
     html += "<script>";
     html += "function updateNumber() {";
@@ -139,23 +182,25 @@ void handleRoot() {
     html += "}";
     html += "setInterval(updateClock, 1000);"; // Update clock every second
     html += "</script></head>";
-    html += "<body><h1>ESP8266 Web Server</h1>";
+    html += "<body><h1>7-Segment</h1>";
+    html += "<body><h3 style='font-style: italic;'>Say it with gears</h3>";
+    html += "<body><h5>esp8266</h5>";
     html += "<label class='switch'><input type='checkbox' onclick='switchMode()'><span class='slider'></span></label>";
     html += "<p>Mode: <span id='currentMode'>Tuning</span></p>";
     html += "<div id='number-section'>";
-    html += "<label for='digit1'>Box nr 1</label>";
+    html += "<label for='digit1'>Digit 1</label>";
     html += "<input type='range' id='digit1' min='0' max='9' value='0' oninput='updateNumber()'><br>";
-    html += "<label for='digit2'>Box nr 2</label>";
+    html += "<label for='digit2'>Digit 2</label>";
     html += "<input type='range' id='digit2' min='0' max='9' value='0' oninput='updateNumber()'><br>";
-    html += "<label for='digit3'>Box nr 3</label>";
+    html += "<label for='digit3'>Digit 3</label>";
     html += "<input type='range' id='digit3' min='0' max='9' value='0' oninput='updateNumber()'><br>";
-    html += "<label for='digit4'>Box nr 4</label>";
+    html += "<label for='digit4'>Digit 4</label>";
     html += "<input type='range' id='digit4' min='0' max='9' value='0' oninput='updateNumber()'><br>";
     html += "<p>Chosen Number: <span id='chosenNumber'>0000</span></p>";
     html += "<button class='button' onclick='sendNumber()'>Send</button>";
     html += "</div>";
     html += "<div id='clock' style='display:none;'>00:00:00</div>";
-    html += "<div id='animation' class='rotate' style='display:none;'>&#9881;</div>";
+    html += "<div id='animation' class='loader' style='display:none;'></div>";
     html += "</body></html>";
     server.send(200, "text/html", html);
 }
@@ -165,7 +210,7 @@ void handleSet() {
     if (currentMode == Tuning) {
         if (server.hasArg("num")) {
             String number = server.arg("num");
-            if (number.length() == 4 && number.toInt() != 0) {
+            if (number.length() == 4) {
                 int digit1 = number.substring(0, 1).toInt();
                 int digit2 = number.substring(1, 2).toInt();
                 int digit3 = number.substring(2, 3).toInt();
